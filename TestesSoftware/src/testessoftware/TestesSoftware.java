8baf981a19fbf3be9c5faae2c69d60f5bee62ae1
Marco Tereso <marco.tereso@gmail.com>
2022-09-15 16:53:55 +0100

    multiplos de varios valores concluido

--- TestesSoftware/src/testessoftware/TestesSoftware.java
+++ TestesSoftware/src/testessoftware/TestesSoftware.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package testessoftware;
 
-/**
- *
- * @author mtereso
- */
-public class TestesSoftware {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class TestesSoftware {
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        int n;
+        Scanner ler = new Scanner(System.in);
+        //pedir valor ao utilizador
+        System.out.println("Insira um valor inteiro");
+        n = ler.nextInt();
+        //verifica se é par ou ímpar
+        if(n%2==0)
+           System.out.println(n+" é par\n"+n+" é múltiplo de 2"); 
+        else
+            System.out.println(n+" é ímpar");
+        if(n%3==0)
+            System.out.println(n+" é múltiplo de 3");
+        if(n%5==0)
+            System.out.println(n+" é múltiplo de 5");
+        if(n%7==0 && n%14==0)
+            System.out.println(n+" é múltiplo de 7 e de 14");
+        if(n%13==0 || n%19==0)
+            System.out.println(n+" é múltiplo de 13 ou de 19");    
+    }  
 }
